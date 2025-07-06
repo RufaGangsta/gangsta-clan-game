df04b51cf311fa52237a1c85fc61a476a8fa7acb
RufaGangsta <eseninaketerina27@gmail.com>
2025-07-06 13:08:20 +0300

    main.js

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
+// ==== Language loader ====
+let currentLang = 'ua'; // 'en', 'ru' можна вибрати тут
+
+async function loadLang() {
+  const response = await fetch('lang.json');
+  const langs = await response.json();
+  const lang = langs[currentLang];
+
+  document.getElementById('title').textContent = lang.title;
+  document.getElementById('spin-btn').textContent = lang.spin;
+  document.getElementById('nft-won-text').textContent = lang.nft_won;
+}
+window.onload = loadLang;
+
 document.getElementById('spinBtn').addEventListener('click', () => {
   const resultEl = document.getElementById('result');
   const nftEl = document.getElementById('nftDrop');
   const rand = Math.random();
   if (rand < 0.0001) { // 0.01% chance
-    nftEl.textContent = '🎴 Випало NFT-герой!';
+    nftEl.textContent = '🎴 Та ну н@х во подгон NFT-герой!';
   } else if (rand < 0.3) {
-    resultEl.textContent = '💰 Виграш: ' + (Math.floor(Math.random()*100) + 1) + ' монет';
+    resultEl.textContent = '💰 Сюда бLя,бабос: ' + (Math.floor(Math.random()*100) + 1) + ' монет';
   } else {
-    resultEl.textContent = '😢 Програш';
+    resultEl.textContent = '😢 Голяк';
   }
 });
