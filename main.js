337817dc83925a4b04d6adb07ccd6d86e344f5a0
RufaGangsta <eseninaketerina27@gmail.com>
2025-07-11 09:29:34 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 // main.js handles UI interactions
 document.getElementById('spinBtn').addEventListener('click', () => {
   const outcome = GameLogic.spin();
-  document.getElementById('slot1').innerText = outcome.slots[0];
-  document.getElementById('slot2').innerText = outcome.slots[1];
-  document.getElementById('slot3').innerText = outcome.slots[2];
+
+  document.getElementById('slot1').innerHTML = `<img src="${outcome.slots[0]}" class="slot-icon">`;
+  document.getElementById('slot2').innerHTML = `<img src="${outcome.slots[1]}" class="slot-icon">`;
+  document.getElementById('slot3').innerHTML = `<img src="${outcome.slots[2]}" class="slot-icon">`;
+
   document.getElementById('result').innerText = outcome.message;
 });
